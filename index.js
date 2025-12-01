d623f498b06983e9b6682b70a940e022a2a90f88
Georgnapr <georgnapr@gmail.com>
2025-12-01 13:26:56 +0300

    проверка обновления документации 2

--- index.js
+++ index.js
@@ -66,5 +66,11 @@ export function handleButtonClick(event) {
     currentInput += value;
     display.textContent = currentInput;
 }
-
+/**
+ * Вывод 123 в консоль
+ * @param {Event} event Выводит 123 в консоль при вызове.
+ */
+function log123 () {
+    console.log('123');
+}
 buttonsContainer.addEventListener('click', handleButtonClick);
